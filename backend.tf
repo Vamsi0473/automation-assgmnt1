fae0f0cf7a9be1ff9d2d3d0479bace8a7c5c6a0c
kvineethreddy <keesarivineethreddy@gmail.com>
2021-07-02 17:23:12 -0400

    latest changes backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstateRG"
-    storage_account_name = "tfstateassignment1_group7"
+    storage_account_name = "tfstateassign1group7"
     container_name       = "tfstatefiles"
     key                  = "tfstate"
   }
--- modules/common/outputs.tf
+++ modules/common/outputs.tf
@@ -10,5 +10,5 @@ output "group7-recovery_services_vault" {
 
 output "group7-storage_account" {
   description = "storage account data"
-  value       = azurerm_storage_account.group7-storage_account
+  value       = azurerm_storage_account.group7-storage_account.name
 }
